af1ef2f9d4083b01f158894c8c107afaee7d7835
angl <luckyan315@qq.com>
2014-02-17 11:26:19 +0800

    init test files

---
+++ public/ws.js
@@ -0,0 +1,20 @@
+/**
+ * WebSocket wrapper for client side
+ * Copyright (C) 2014 guanglin.an (lucky315.an@gmail.com)
+ * 
+ */
+
+"use strict";
+
+function WebSocket(){
+  
+}
+
+(function(){
+
+  this.init = function(){
+    //
+    alert('init websocket');
+  };
+  
+}).call(WebSocket.prototype);
