ec39c2fea514fd350f26745bc340f129f163f338
luckyan315 <luckyan315@qq.com>
2014-02-17 23:25:04 +0800

    Simple implementaion of EventEmitter

--- public/event.js
+++ public/event.js
@@ -16,18 +16,48 @@ function EventEmitter(){
 
 (function(){
 
-  this.emit = function(){
-    
+  this.emit = function(event){
+    if (typeof event !== 'string') throw TypeError('event must be a string');
+    if (!this.$events || !this.$events[event]) return;
+
+    for (var i = 0; i < this.$events[event].length; ++i)
+    {
+      var fn = this.$events[event];
+      switch (arguments.length) {
+       case 1:
+        fn.call(this);
+        break;
+       case 2:
+        fn.call(this, arguments[1]);
+        break;
+       case 3:
+        fn.call(this, arguments[1], arguments[2]);
+        break;
+      default:
+        //slow emitter
+        var args = Array.prototype.slice.call(arguments, 1);
+        fn.apply(this, args);
+      }
+      
+    }
   };
 
-  this.addListener = function(){
-    
+  this.addListener = function(event, listener){
+    if (typeof event !== 'string') throw TypeError('event must be a string!');
+    if (typeof listener !== 'function') throw TypeError('listener is needed!');
+
+    this.$events = this.$events || {};
+    this.$events[event] = this.$events[event] || [];
+
+    this.$events[event].push(listener);
   };
 
   this.on = this.addListener;
 
-  this.removeListener = function(){
-    
+  this.removeListeners = function(event){
+    if (this.$events[event]) {
+      delete this.$events[event];
+    }
   };
   
 }).call(EventEmitter.prototype);
