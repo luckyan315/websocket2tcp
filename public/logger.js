a7e7e3f84e538c13c7b0565d3932f7c401dda818
angl <luckyan315@qq.com>
2014-02-17 18:09:37 +0800

    enhance logger module

--- public/logger.js
+++ public/logger.js
@@ -1,11 +1,20 @@
+/**
+ * common logger module 
+ * Copyright (C) 2014 guanglin.an (lucky315.an@gmail.com)
+ * 
+ */
+
+"use strict";
+
 var logger = {};
 
 logger.init = function(level){
-  logger.debug = logger.info = logger.warn = logger.error = function(msg) {};
+  logger.debug = logger.log = logger.info = logger.warn = logger.error = function(msg) {};
 
   Array.prototype.shift.call(arguments);
   switch(level){
     case 'debug': logger.debug = function(){ console.log.apply(console, arguments);};
+    case 'log': logger.log = function(){ console.log.apply(console, arguments);};
     case 'info': logger.info = function(){ console.info.apply(console, arguments);};
     case 'warn': logger.warn = function(){ console.warn.apply(console, arguments);};
     case 'error': logger.error = function(){ console.error.apply(console, arguments);};
