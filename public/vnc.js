d0714d2ea3acc3abf147b11f43ddfc880d5a5928
luckyan315 <luckyan315@qq.com>
2014-04-12 23:31:25 +0800

    add vnc client

---
+++ public/vnc.js
@@ -0,0 +1,23 @@
+/**
+ * a vnc client impl via HTML5 canvas
+ *
+ * Copyright (C) 2014 guanglin.an (lucky315.an@gmail.com)
+ * 
+ */
+
+"use strict";
+
+function VNC(){
+  EventEmitter.call(this);
+
+  logger.debug('create vnc client!!!!');
+};
+
+Utils.inherits(VNC, EventEmitter);
+
+(function(){
+  //public
+
+  //private
+  
+}).call(VNC.prototype);
